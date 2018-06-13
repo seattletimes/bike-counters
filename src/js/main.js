e6da7a4c0e3c54ee1f28c5a50ff7ec7d889de147
Alec Glassford <glassford@cs.stanford.edu>
2018-06-13 11:55:30 -0700

    Redraw sparklines on resize

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 var Vue = require("vue/dist/vue.min");
 require("component-responsive-frame/child");
-var { palette }= require("./lib/colors");
+var { palette } = require("./lib/colors");
+var debounce = require("./lib/debounce");
 
 // TODO: double check these meanings with the city
 var decipher = {
@@ -59,45 +60,55 @@ Vue.component('spark-line', {
   },
   props: ['weekly'],
   template: '<canvas></canvas>',
-  mounted() {
-    var canvas = this.$el;
-    var context = canvas.getContext('2d');
-    canvas.width = canvas.clientWidth;
-    canvas.height = canvas.clientHeight;
-    var abbrevs = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
-    var xs = [1, 2, 3, 4, 5, 6, 7].map(n => Math.round(n * canvas.width / 8));
+  methods: {
+    draw() {
+      var canvas = this.$el;
+      var context = canvas.getContext('2d');
+      canvas.width = 0;
+      canvas.height = 0;
+      canvas.width = canvas.clientWidth;
+      canvas.height = canvas.clientHeight;
+      var abbrevs = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
+      var xs = [1, 2, 3, 4, 5, 6, 7].map(n => Math.round(n * canvas.width / 8));
 
-    // Draw day of week letters
-    context.font = '16px sans-serif'; // TODO: fix font
-    context.textAlign = 'center';
-    abbrevs.forEach((abbrev, i) => {
-      var x = xs[i];
-      context.fillText(abbrev, x, canvas.height - 5);
-    });
-
-    // Draw baseline (20 pixels room at bottom for text)
-    context.beginPath();
-    context.moveTo(0, canvas.height - 20);
-    context.lineTo(canvas.width, canvas.height - 20);
-    context.stroke();
-
-    // Draw each line
-    this.weekly.forEach((row, dirNum) => {
-      var ys = row.values.map((numBikes) => {
-        var scaled = numBikes / this.maxDaily;
-        // Now map from [0,1] space to [canvas.height - 20, 10]
-        // i.e. 20 padding at bottom (for letters), 10 padding at top
-        return Math.round((canvas.height - 20) * (1 - scaled) + 10 * scaled);
+      // Draw day of week letters
+      context.font = '16px sans-serif'; // TODO: fix font
+      context.textAlign = 'center';
+      abbrevs.forEach((abbrev, i) => {
+        var x = xs[i];
+        context.fillText(abbrev, x, canvas.height - 5);
       });
-      context.strokeStyle = dirNum ? palette.stDarkBlue : palette.stDarkRed;
+
+      // Draw baseline (20 pixels room at bottom for text)
       context.beginPath();
-      xs.forEach((x, i) => {
-        var y = ys[i];
-        if (i === 0) context.moveTo(x, y);
-        else context.lineTo(x, y);
-        context.fillRect(x - 2, y - 2, 4, 4);
-      });
+      context.moveTo(0, canvas.height - 20);
+      context.lineTo(canvas.width, canvas.height - 20);
       context.stroke();
+
+      // Draw each line
+      this.weekly.forEach((row, dirNum) => {
+        var ys = row.values.map((numBikes) => {
+          var scaled = numBikes / this.maxDaily;
+          // Now map from [0,1] space to [canvas.height - 20, 10]
+          // i.e. 20 padding at bottom (for letters), 10 padding at top
+          return Math.round((canvas.height - 20) * (1 - scaled) + 10 * scaled);
+        });
+        context.strokeStyle = dirNum ? palette.stDarkBlue : palette.stDarkRed;
+        context.beginPath();
+        xs.forEach((x, i) => {
+          var y = ys[i];
+          if (i === 0) context.moveTo(x, y);
+          else context.lineTo(x, y);
+          // context.fillRect(x - 2, y - 2, 4, 4);
+        });
+        context.stroke();
+      });
+    },
+  },
+  mounted() {
+    this.draw();
+    window.addEventListener('resize', () => {
+      debounce(this.draw)();
     });
   },
 })
