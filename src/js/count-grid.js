b5762519a88bd47bceb1d488bc81140c595af73e
Alec Glassford <glassford@cs.stanford.edu>
2018-06-13 12:03:31 -0700

    Refactor Vue components

---
+++ src/js/count-grid.js
@@ -0,0 +1,15 @@
+module.exports = function countGrid(decipher, maxVal, commafy) {
+  return {
+    data() {
+      return {
+        decipher,
+        maxVal,
+      };
+    },
+    props: ['totals'],
+    methods: {
+      commafy,
+    },
+    template: require('./_count-grid.html'),
+  };
+};
