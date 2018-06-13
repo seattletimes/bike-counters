4ac04dd93532579d59efcfb7a9fb4db61c321d85
Alec Glassford <glassford@cs.stanford.edu>
2018-06-13 15:27:30 -0700

    Simplify sparklines

--- src/js/spark-line.js
+++ src/js/spark-line.js
@@ -3,11 +3,6 @@ var debounce = require("./lib/debounce");
 
 module.exports = function sparkLine({maxDaily}) {
   return {
-    data() {
-      return {
-        maxDaily,
-      };
-    },
     props: ['weekly'],
     template: '<canvas></canvas>',
     methods: {
@@ -21,27 +16,14 @@ module.exports = function sparkLine({maxDaily}) {
         var abbrevs = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
         var xs = [1, 2, 3, 4, 5, 6, 7].map(n => Math.round(n * canvas.width / 8));
 
-        // Draw day of week letters
-        context.font = '16px sans-serif'; // TODO: fix font
-        context.textAlign = 'center';
-        abbrevs.forEach((abbrev, i) => {
-          var x = xs[i];
-          context.fillText(abbrev, x, canvas.height - 5);
-        });
-
-        // Draw baseline (20 pixels room at bottom for text)
-        context.beginPath();
-        context.moveTo(0, canvas.height - 20);
-        context.lineTo(canvas.width, canvas.height - 20);
-        context.stroke();
-
         // Draw each line
         this.weekly.forEach((row, dirNum) => {
+          var maxDaily = Math.max(... row.values);
+          var minDaily = Math.min(... row.values);
           var ys = row.values.map((numBikes) => {
-            var scaled = numBikes / this.maxDaily;
-            // Now map from [0,1] space to [canvas.height - 20, 10]
-            // i.e. 20 padding at bottom (for letters), 10 padding at top
-            return Math.round((canvas.height - 20) * (1 - scaled) + 10 * scaled);
+            var scaled = (numBikes - minDaily) / (maxDaily - minDaily);
+            // Now map from [0,1] space to [canvas.height, 0]
+            return Math.round((canvas.height) * (1 - scaled));
           });
           context.strokeStyle = dirNum ? palette.stDarkBlue : palette.stDarkRed;
           context.beginPath();
@@ -49,7 +31,6 @@ module.exports = function sparkLine({maxDaily}) {
             var y = ys[i];
             if (i === 0) context.moveTo(x, y);
             else context.lineTo(x, y);
-            // context.fillRect(x - 2, y - 2, 4, 4);
           });
           context.stroke();
         });
