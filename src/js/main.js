b63ac109f359f11af899ff2326a5b11ca7cb37d2
Alec Glassford <glassford@cs.stanford.edu>
2018-06-19 15:50:45 -0700

    Reorder rows (feat. magical Vue sorting animations

--- src/js/main.js
+++ src/js/main.js
@@ -18,13 +18,39 @@ var app = new Vue({
   data: {
     counterNames: [ '2nd-ave', '26th-ave', '39th-ave', '58th-st', 'broadway',
       'burke-gilman', 'elliott-bay', 'fremont-bridge', 'mts', 'spokane-st'],
+    sortedBy: null,
+    sortOrder: null, // -1 for descending, 1 for ascending
     bikeCounts: window.bikeCounts,
     drilldown: null,
+    sortHeaders: ['May 2016 - Apr. 2017', 'May 2017 - Apr. 2018', 'Change'],
   },
   methods: {
     toggleDrilldown(counter) {
       if (this.drilldown === counter) this.drilldown = null;
       else this.drilldown = counter;
+    },
+    sortRows(index) {
+      // Case: reverse sort order
+      if (index === this.sortedBy) {
+        this.counterNames.reverse();
+        this.sortOrder *= -1;
+        return;
+      }
+
+      // Case: sort by new column
+      this.counterNames.sort((a, b) => {
+        if (index === 2) {
+          var changeB = (this.bikeCounts[b].totals[1] - this.bikeCounts[b].totals[0]) / this.bikeCounts[b].totals[0];
+          var changeA = (this.bikeCounts[a].totals[1] - this.bikeCounts[a].totals[0]) / this.bikeCounts[a].totals[0];
+          return changeB - changeA;
+        }
+        return this.bikeCounts[b].totals[index] - this.bikeCounts[a].totals[index];
+      });
+      this.sortedBy = index;
+      this.sortOrder = -1;
     }
   },
+  created() {
+    this.sortRows(0);
+  }
 });
