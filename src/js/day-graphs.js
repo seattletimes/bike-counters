375056f3233917351f653e180242c5057b301231
Alec Glassford <glassford@cs.stanford.edu>
2018-06-21 10:42:56 -0700

    More labeling on day graphs

--- src/js/day-graphs.js
+++ src/js/day-graphs.js
@@ -1,50 +1,86 @@
 var { palette } = require("./lib/colors");
-var { canvasDraw, canvasSetup } = require("./util");
+var { canvasDraw, canvasSetup, decipher } = require("./util");
 
-var drawDay = function drawDay(dayType, data, parent) {
-  var canvas = parent.querySelector(`.${dayType}`);
+var padding= {
+  top: 1,
+  bottom: 5,
+};
+
+var drawDay = function drawDay(dayType) {
+  var data = this[dayType];
+  var canvas = this.$el.querySelector(`.${dayType}`);
   var context = canvasSetup(canvas);
+  this.canvasWidth = canvas.width;
 
   // Temporary..
+  var baselineY = canvas.height - padding.bottom;
 
+  //Shade left and right
+  context.fillStyle = '#eee';
+  context.fillRect(0, padding.top, this.xs[9], baselineY);
+  context.fillRect(this.xs[17], padding.top, this.xs[23] - this.xs[17], baselineY);
+
+  // Draw baseline
   context.lineWidth = 1;
   context.strokeStyle = palette.dfMiddleGray;
   context.beginPath();
-  context.moveTo(0, canvas.height - 20);
-  context.lineTo(canvas.width, canvas.height - 20);
+  context.moveTo(0, baselineY);
+  context.lineTo(canvas.width, baselineY);
   context.stroke();
 
   // max of both directions
   var maxVal = Math.max(... data[0].values.concat(data[1].values));
+
+  // Draw actual lines
+  context.lineWidth = 2;
   data.forEach((dirData, dirNum) => {
     var ys = dirData.values.map((numBikes) => {
       var scaled = numBikes / maxVal;
-      // Map [0,1] to [canvas.height - 20, 1]
-      return (1 - scaled) * (canvas.height - 20) + scaled;
+      // Map [0,1] to [baselineY, 1]
+      return (1 - scaled) * baselineY + scaled * padding.top;
     });
     context.strokeStyle = dirNum  ? palette.stDarkBlue : palette.stDarkRed;
-    context.lineWidth = 2;
     context.beginPath();
     ys.forEach((y, i) => {
-      var x = i * canvas.width / (ys.length - 1);
+      var x = this.xs[i];
       if (i === 0) context.moveTo(x, y);
       else context.lineTo(x, y);
     });
     context.stroke();
+  });
+
+  // Ticks
+  context.fillStyle = palette.dfMiddleGray;
+  this.xs.forEach((x) => {
+    context.fillRect(x - 1, baselineY - 2, 2, 4);
   });
 };
 
 module.exports = function dayGraphs() {
   return {
     data() {
-      return { resizeListener: null };
+      return {
+        ticks: {
+          9: '9 a.m.',
+          17: '5 p.m.',
+        },
+        canvasWidth: 0,
+        decipher,
+        resizeListener: null
+      };
     },
     props: ['weekday', 'weekend'],
-    template: '<div class="day-graphs"><canvas class="weekday"></canvas><canvas class="weekend"></canvas></div>',
+    computed: {
+      xs() {
+        return this.weekday[0].values.map((_, i) =>
+          this.canvasWidth * i / (this.weekday[0].values.length - 1));
+      },
+    },
+    template: require('./_day-graphs.html'),
     methods: {
       draw() {
-        drawDay('weekday', this.weekday, this.$el);
-        drawDay('weekend', this.weekend, this.$el);
+        drawDay.call(this, 'weekday');
+        drawDay.call(this, 'weekend');
       },
     },
     mounted: canvasDraw,
